aa6f1d255a8672c4d63d34a4593fcbac86b50538
JunmoYang <115628899+Juunnmmoo@users.noreply.github.com>
2025-03-29 17:02:47 +0900

    Add : ChatWidgets

---
+++ Source/BullsAndCows/Public/UI/MOChatBox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MOChatBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLSANDCOWS_API UMOChatBox : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BullsAndCows/Public/UI/MOChatInput.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MOChatInput.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLSANDCOWS_API UMOChatInput : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
